1ae9ec4be5fdc28bbef88482fc79a8ccda11d3cc
Yuuuk94 <yuuuk94@gmail.com>
2022-05-03 02:24:32 +0900

    20220502
    index only

--- js/common.js
+++
@@ -1,8 +0,0 @@
-$('body')
-.prepend('<header></header>')
-.append('<footer></footer>');
-
-// $('header').load(url[, data][, complete]);
-// $('body').prepend().load('inc.html header');
-$('header').load('inc.html header >div');
-$('footer').load('inc.html footer >div');
